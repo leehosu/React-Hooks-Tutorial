98aa508c2f96468989942edafa3de44c6255a7ea
l2s <48622670+leehosu@users.noreply.github.com>
2020-02-14 00:50:46 +0900

    study the Hooks about useState, useEffect, useContext

---
+++ hooks/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Counter from './Component/Counter';
+import Info from './Component/Info';
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div className="App">
+      <Counter />
+      <div className = "visible">
+        <button onClick = {() => {setVisible(!visible);}} >
+          {visible ? '보이기' : '숨기기'}
+        </button>
+      </div>
+      <div className = "App-Info">
+        {visible && <Info />}
+      </div>
+    </div>
+  );
+}
+
+export default App;
